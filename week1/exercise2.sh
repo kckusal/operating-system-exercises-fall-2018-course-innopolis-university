89a9b21aaf750310f63cb6bbdcf3656467f7f729
Kusal KC <kckusal99@gmail.com>
2018-08-25 19:01:27 +0300

    Create exercise2.sh
    The following was the exact task description for exercise 2:
    
       Try some commands and save history to “~/week1/ex2.txt”.

---
+++ week1/exercise2.sh
@@ -0,0 +1,12 @@
+#Exercise Description:
+#  Try some commands and save history to “~/week1/ex2.txt”.
+#
+#Exercise Solution:
+
+whoami
+hostname
+mkdir ~/apps
+ls > ~/apps/list.txt
+mkdir ~/week1
+cd ~/week1
+history > ex2.txt
