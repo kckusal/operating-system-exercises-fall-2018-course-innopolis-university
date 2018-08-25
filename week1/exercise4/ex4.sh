dca9b1d18e6c6be5f1c2ed7841508e4588d5a296
Kusal KC <kckusal99@gmail.com>
2018-08-25 19:19:03 +0300

    Update ex4.sh
    Compile and run the "Hello World" program created.

--- week1/exercise4/ex4.sh
+++ week1/exercise4/ex4.sh
@@ -3,4 +3,12 @@
 #  Create source file: gedit ~/week1/main.c
 
 #Exercise Solution:
+
+#Create and open the file for "Hello World" program
 gedit ~/main.c
+
+#Compile the "Hello World" program created
+gcc main.c -o ex4
+
+#Run the program
+./ex4
