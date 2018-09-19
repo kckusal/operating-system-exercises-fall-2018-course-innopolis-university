6f3efbc797ae8160a91424d242d211abd26bf839
Kusal KC <kckusal99@gmail.com>
2018-09-19 18:00:33 +0300

    Script for Incrementing & Appending to a File

---
+++ week5/exercise2.sh
@@ -0,0 +1,11 @@
+# Exercise Description:
+#   Write a shell script that produces a file of sequential numbers by reading the last number in the file,
+#   adding 1 to it, and then appending it to the file. Run one instance of the script in the background and one
+#   in the foreground, each accessing the same file.
+
+# Assume that there's a file called "input.txt" in the same directory as this script file
+
+in=input.txt
+awk -v last="$last" 'END { printf "\n%s",$0=$0+1 }' $in >> $in
+
+# This will take the last number in the file, add 1 to it and append the resulting value in the file.
