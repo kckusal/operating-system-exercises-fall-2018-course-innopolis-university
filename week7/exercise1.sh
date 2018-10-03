89d30458bb53d19c40904c8e7b856d2e1e3eea89
Kusal KC <kckusal99@gmail.com>
2018-10-03 16:12:06 +0300

    Using 'size' shell command to get bss, text segment & data segment info of an object file

--- week7/exercise1.sh
+++ week7/exercise1.sh
@@ -2,8 +2,8 @@
 #   Use size shell command to determine the size of text, data and bss segments of any of your programs.
 #   Save the output to file ex1.txt
 
-# Compile a requisite C program named 'ex1_requisite.c' and create executable 'ex1_requisite'
-gcc ex1_requisite.c -o ex1_requisite
+# Compile a prerequisite C program named 'ex1_prerequisite.c' and create executable 'ex1_prerequisite'
+gcc ex1_prerequisite.c -o ex1_prerequisite
 
 # Output size of the bss, text segment and data segment for created object file
-size ex1_requisite > ex1.txt
+size ex1_prerequisite > ex1.txt
