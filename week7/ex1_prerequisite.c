f297c346d5412f20b2b333ca345f25202fea23e5
Kusal KC <kckusal99@gmail.com>
2018-10-03 16:09:31 +0300

    Sample C program used by exercise1.sh

---
+++ week7/ex1_prerequisite.c
@@ -0,0 +1,10 @@
+/* Just an example C program used as a prerequisite by exercise1.sh file to follow. */
+
+#include <stdio.h> 
+  
+int pi = 3;
+  
+int main() { 
+    static int i;
+    return 0; 
+} 
