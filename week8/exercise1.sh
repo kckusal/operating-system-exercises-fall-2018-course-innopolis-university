7e88025a3843d07affe05ab188ed2ec9356e3db2
Kusal KC <kckusal99@gmail.com>
2018-10-10 14:03:18 +0300

    Generate instantaneous memory report by running `free -t -h` in the shell

---
+++ week8/exercise1.sh
@@ -0,0 +1,13 @@
+# EXERCISE DESCRIPTION:
+#   Run `free -t -h` in the shell or `vm_stat` on macOS.
+
+# Note that the script below was tested in GNU bash, version 4.4.12(1)-release (x86_64-pc-linux-gnu)
+
+free -t -h
+
+# Result during run (report below is instantaneous and differs based on PC specifications and processor in use):
+
+#               total        used        free      shared  buff/cache   available
+# Mem:            25G        8.3G        5.7G        264M         11G         16G
+# Swap:            0B          0B          0B
+# Total:          25G        8.3G        5.7G
